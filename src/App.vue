d3cea39e011ff279a35c4e7cb5cde7b7f249c6b7
Jordan Chavez <60524233+JordanChavez21@users.noreply.github.com>
2022-02-24 11:52:06 -0700

    asf

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,56 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <section class="todoapp">
+    <header>
+      <h1>Treas</h1>
+      <input
+        type="text"
+        class="new-todo"
+        autofocus
+        autocomplete="off"
+        placeholder="Escribe tu tarea aqui"
+        v-model="newTodo"
+        @keyup.enter="addTodo()"
+      />
+    </header>
+    <section>
+      <ul>
+        <li class="todo-list" v-for="todo in todos" :key="todo.id">
+          <input class="toggle" type="checkbox" />
+          {{ todo.title }}
+        </li>
+      </ul>
+    </section>
+  </section>
 </template>
+<script>
+/* eslint-disable */
+export default {
+  data: () => ({
+    todos: [],
+    newTodo: "",
+  }),
+  methods: {
+    addTodo() {
+      const value = this.newTodo && this.newTodo.trim();
 
+      const todoItem = {
+        id: this.todos.length + 1,
+        title: value,
+        completed: false,
+      };
+
+      if (!value) {
+        return
+      }
+
+      this.todos.push(todoItem);
+    },
+  },
+};
+</script>
 <style>
+@import "./styles/todomvc-base.css";
+@import "./styles/todomvc-index.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
